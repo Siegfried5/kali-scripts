a484709d8fc2da13e08d8f93b985ccc9477f46bc
Siegfried5 <rudolph.ponce745@gmail.com>
2015-09-29 07:28:10 -0700

    changed macchanger script

--- macchanger.sh
+++ macchanger.sh
@@ -1,7 +1,8 @@
 ifconfig wlan0 down
-ifconfig wlan1 down
 macchanger -a wlan0
-macchanger -a wlan1
 ifconfig wlan0 up
-ifconfig wlan1 up
+ifconfig wlan1 down
 
+ifconfig wlan1 down
+macchanger -a wlan1
+ifconfig wlan1 up
