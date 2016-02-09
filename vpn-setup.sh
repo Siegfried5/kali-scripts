f017d4fa8512a97c170f40153e63fd880d2ef7a3
root <root@localhost.localdomain>
2016-02-08 22:43:02 -0700

    added armitage-setup + vpn-setup

---
+++ vpn-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Author Rudolph Ponce
+#
+# Description: This Script will enable vpn setup on kali linux 2.0
+# Creds got to http://www.blackmoreops.com/2015/03/01/setup-vpn-on-kali-linux/
+# :) happy hacking
+
+
+
+aptitude -r install network-manager-openvpn-gnome network-manager-pptp network-manager-pptp-gnome network-manager-strongswan network-manager-vpnc network-manager-vpnc-gnome
