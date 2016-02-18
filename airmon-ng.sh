6eb39a0195c18d666cb06bfd471f74c259f04b43
Siegfried5 <rudolph.ponce745@gmail.com>
2016-02-18 12:25:52 -0700

    mod airmon-ng.sh

--- airmon-ng.sh
+++ airmon-ng.sh
@@ -1,3 +1,18 @@
+#! /bin/bash
+#Rudolph Ponce
+#Thu Feb 18 12:19:45 MST 2016
+
+echo "We are going to run these commands:
 service network-manager stop
+airmon-ng check kill
 airmon-ng start wlan0
-airodump-ng wlan0mon
+airodump-ng wlan0mon -w raw_dump
+"
+
+service network-manager stop
+airmon-ng check kill
+airmon-ng start wlan0
+echo ""
+echo "Were able to start monitor mode: device name is wlan0mon"
+sleep 5
+airodump-ng wlan0mon -w raw_dump
