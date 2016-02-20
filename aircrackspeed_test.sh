cf3a4e2bfda4336880d76535314be78dccc03b14
Siegfried5 <rudolph.ponce745@gmail.com>
2016-02-19 21:01:44 -0700

    added aircrackspeed_test.sh

---
+++ aircrackspeed_test.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+#Rudolph Ponce
+#Thu Feb 18 12:11:34 MST 2016
+
+echo "Were are going to test how fast you can Crack WPA hash with aircrack-ng"
+echo ''
+aircrack-ng -S
+echo "This is the amount of keys you can crack per seconds"
